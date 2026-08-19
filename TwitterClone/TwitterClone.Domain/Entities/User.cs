002f6eda508394a32009c88fc4e9175542e39a6e
Laju <lajul943@gmail.com>
2026-08-19 22:34:17 +0600

    Update user entity

--- TwitterClone/TwitterClone.Domain/Entities/User.cs
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace TwitterClone.Domain.Entities
 {
-    internal class user
+    public class User
     {
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+        
+        public Guid ID
+        {
+            get { return _id; }
+        }
+
+        public string FirstName {
+            get { return _firstName; } 
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
     }
 }
